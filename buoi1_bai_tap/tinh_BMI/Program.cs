aaed2ad27f9885048d21e55765d8f64baa7dae09
Son Trinh <tqson92@gmail.com>
2025-09-29 12:47:19 +0700

    upload - bai tap buoi 1

---
+++ buoi1_bai_tap/tinh_BMI/Program.cs
@@ -0,0 +1,20 @@
+﻿// viết chương trình nhập vào cân nặng và chiều cao, sau đó tính và hiển thị chỉ số BMI
+
+// input
+Console.Write($@"Nhập vào cân nặng (theo kg): ");
+double canNang = Convert.ToDouble(Console.ReadLine());
+Console.Write($@"Nhập vào chiều cao (theo mét): ");
+double chieuCao = Convert.ToDouble(Console.ReadLine());
+
+// output
+double bmi = 0;
+
+// process
+bmi = canNang / (chieuCao * chieuCao);
+
+// xuất output
+Console.WriteLine($@"
+    Cân nặng: {canNang} kg
+    Chiều cao: {chieuCao} m
+    Chỉ số Body Mass Index: {bmi}
+");

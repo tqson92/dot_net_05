945d7356e9f291a253008f47d1e0bb25f9a587b9
Son Trinh <tqson92@gmail.com>
2025-10-10 19:40:42 +0700

    update bai tap buoi 2

---
+++ buoi2_bai_tap/mua_trong_nam/Program.cs
@@ -0,0 +1,24 @@
+﻿// viết chương trình nhập tháng và hiển thị mùa tương ứng
+
+// input
+Console.Write($@"Nhập vào số tháng (từ tháng 1 tới tháng 12): ");
+double soThang = Convert.ToInt16(Console.ReadLine());
+
+// output
+string mua = "";
+
+// process
+mua = soThang switch
+{
+    >= 1 and <= 3 => "xuân",
+    >= 4 and <= 6 => "hạ",
+    >= 7 and <= 9 => "thu",
+    >= 10 and <= 12 => "đông",
+    _ => "không xác định"
+};
+
+// xuất output
+Console.WriteLine($@"
+    Tháng nhập vào: {soThang}
+    Mùa theo tháng là mùa {mua}
+");
---
+++ buoi2_bai_tap/tinh_tuoi_nghia_vu/Program.cs
@@ -0,0 +1,22 @@
+﻿// viết chương trình nhập vào số tuổi của công dân và xuất ra xem có đủ tuổi đi nghĩa vụ quân sự hay không
+
+// input
+Console.Write($@"Nhập vào số tuổi của công dân: ");
+double soTuoi = Convert.ToInt16(Console.ReadLine());
+
+// output
+string kiemTra = "";
+
+// process
+kiemTra = soTuoi switch
+{
+    < 18 => "chưa đủ tuổi tham gia NVQS",
+    >= 18 and <= 27 => "đủ tuổi tham gia NVQS",
+    _ => "quá tuổi tham gia NVQS"
+};
+
+// xuất output
+Console.WriteLine($@"
+    Tuổi của công dân là: {soTuoi}
+    Nên {kiemTra}
+");
